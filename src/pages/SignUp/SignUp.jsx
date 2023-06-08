d6e18025765639b2ebaf67134c8c12d53866e5b1
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-06-08 07:35:06 +0600

    sign up page and create a user added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,96 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../../Hooks/useAuth';
 
 const SignUp = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { createUser} = useAuth();
+    const navigate = useNavigate();
+    const onSubmit = data => {
+        console.log(data);
+
+        // create user
+        createUser(data.email, data.password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            navigate('/');
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    };
     return (
         <div>
-            <h1>sign up</h1>
+            <div className="grid md:grid-cols-2 bg-base-200 my-12 md:w-3/4 mx-auto">
+                <div><img src="https://i.ibb.co/KNXxQcz/login.webp" alt="" /></div>
+
+                <div className="shadow-2xl bg-base-100 p-4">
+                    <h1 className="text-4xl text-center p-4 font-bold">SignUP!</h1>
+                    <form onSubmit={handleSubmit(onSubmit)} className='space-y-2'>
+                        <div className='form-control'>
+                            <label htmlFor="">Name</label>
+                            <input className='input input-bordered' {...register("name", { required: true })} placeholder='Enter your name' />
+                            {errors.name && <span className='mt-3 text-red-600'>Name  is required</span>}
+                        </div>
+                        <div className='form-control'>
+                            <label htmlFor="">photo URL</label>
+                            <input className='input input-bordered' {...register("photoUrl", { required: true })} placeholder='Photo url' />
+                            {errors.photoURL && <span className='mt-3 text-red-600'>Photo URL  is required</span>}
+                        </div>
+                        <div className='form-control'>
+                            <label htmlFor="">Email</label>
+                            <input className='input input-bordered'  {...register("email", { required: true })} placeholder='Enter your Email' />
+                            {errors.email && <span className='mt-3 text-red-600'>Email  is required</span>}
+                        </div>
+                        <div className='form-control'>
+                            <label htmlFor="">Password</label>
+                            <input className='input input-bordered' type='password' {...register("password", {
+                                required: true,
+                                minLength: 8,
+                                maxLength: 20,
+                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                            })} placeholder='Enter your password' />
+                            {errors.password?.type == 'required' && <span className='mt-3 text-red-600'>Password field required</span>}
+                            {errors.password?.type === 'minLength' && <span className='mt-3 text-red-600'>Password must be 8 characters long</span>}
+                            {errors.password?.type === 'maxLength' && <span className='mt-3 text-red-600'>Password must be less than 20 characters</span>}
+                            {errors.password?.type === 'pattern' && <span className='mt-3 text-red-600'>Password must have one upperCase, one lowerCase, one number and one special characters</span>}
+                        </div>
+                        <div className='form-control'>
+                            <label htmlFor="">Confirm Password</label>
+                            <input className='input input-bordered' type='password' {...register("confirm-password", {
+                                required: true,
+                            })} placeholder='Enter your confirm password' />
+                            {errors.password?.type == 'required' && <span className='mt-3 text-red-600'>Confirm Password field required</span>}
+                        </div>
+                        <div className='grid grid-cols-2 gap-4'>
+                            <div className='form-control'>
+                                <label htmlFor="">Gender</label>
+                                <select className='border-2 p-3 rounded-xl' {...register("gender")}>
+                                    <option value="female">female</option>
+                                    <option value="male">male</option>
+                                    <option value="other">other</option>
+                                </select>
+                            </div>
+                            <div className='form-control'>
+                                <label htmlFor="">Phone Number</label>
+                                <input className='input input-bordered' type="number" {...register("PhoneNumber", )} placeholder='Enter your number' />
+                            </div>
+                        </div>
+                        <div className='form-control'>
+                                <label htmlFor="">Address</label>
+                                <input className='input input-bordered' type="text" {...register("address", { min: 60, max: 100 })} placeholder='Enter your address' />
+                            </div>
+
+                        <div className='form-control'>
+                            <input type="submit" className='btn btn-primary' value="SignUP" />
+                        </div>
+                    </form>
+                    <p className='my-4 text-center'><small className='text-red-500 text-center'>Already registered? <Link to='/login' className='underline text-md'>Go to LogIn</Link></small></p>
+                    {/* <SocialSignIn></SocialSignIn> */}
+                </div>
+            </div>
         </div>
     );
 };
