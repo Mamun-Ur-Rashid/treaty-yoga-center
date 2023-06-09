aee262bec0bc530e3163d6cb32b17db09260965a
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-06-09 07:50:41 +0600

    custom hook use axios secure create

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../../firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -41,8 +42,23 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log("current User", currentUser);
-            setLoading(false);
+            // jwt token token
+           if(currentUser){
+            axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+            .then(data => {
+                console.log(data.data.token);
+                localStorage.setItem('access-token', data.data.token);
+                setLoading(false);
+            })
+           }
+           else{
+            localStorage.removeItem('access-token');
+           }
+          
         }) 
+        return () => {
+            return unsubscribe();
+        }
     } , [])
     const authInfo = {
         user, loading, createUser, signIn, signInGoogle, logOut, updateUserProfile
